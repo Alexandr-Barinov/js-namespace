01f812a5665dcc14ca9a150ce7932f9953bf6457
Alexandr-Barinov <32398080+Alexandr-Barinov@users.noreply.github.com>
2017-10-14 18:31:14 -0400

    4th

--- namespace.js
+++ namespace.js
@@ -1,11 +1,8 @@
 var BARI0053 = {
-    
+
     init: function(){  
     var div = document.createElement("div");
     div.className = "box";
     div.textContent = "bari0053";
     document.getElementById("boxes").appendChild("box");
-    console.log(yes);
-    };
-
 };

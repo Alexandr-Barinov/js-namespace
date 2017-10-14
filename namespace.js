7b6feb15a96911d15e60b913f77db375162c7295
Alexandr-Barinov <32398080+Alexandr-Barinov@users.noreply.github.com>
2017-10-14 19:05:14 -0400

    FIX

--- namespace.js
+++ namespace.js
@@ -7,6 +7,18 @@ var BARI0053 = {
     box.textContent = "bari0053";
     document.getElementById("boxes").appendChild("box");
 
+    div.addEventListener("mouseover", mouseover);
+    div.addEventListener("mouseout", mouseout);
+
+    function mouseover(ev){
+    ev.currentTarget.toggle("highlight");
+    };
+
+    function mouseout(ev){
+    ev.currentTarget.toggle("highlight");
+    };
+
+        
     }
     
 };

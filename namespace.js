5ba97c482027e856d414d32650d972c3fb2caead
Alexandr-Barinov <32398080+Alexandr-Barinov@users.noreply.github.com>
2017-10-16 00:04:08 -0400

    sans-click

--- namespace.js
+++ namespace.js
@@ -3,8 +3,21 @@ var BARI0053 = {
     init: function(){
 
     var box = document.createElement("div");
-    box.className = "box";
+    box.classList.add("box");
     box.textContent = "bari0053";
-    document.getElementById("boxes").appendChild("box");
-    }
-};
+    document.getElementById("boxes").appendChild(box);
+
+    div.addEventListener("mouseover", mouseover);
+    div.addEventListener("mouseout", mouseout);
+    div.addEventListener("click", click);
+    };
+    
+    function mouseover(ev){
+    ev.currentTarget.classList.toggle("highlight");
+    };
+
+    function mouseout(ev){
+    ev.currentTarget.classList.toggle("highlight");
+    };
+        
+    };
